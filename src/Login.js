9c23bf4cab14c177dec6fe1832b918a9696202a1
SanjuDeveloper <bhattsanju41@gmail.com>
2021-11-20 12:31:37 +0530

    Implement Login api

--- src/Login.js
+++ src/Login.js
@@ -4,16 +4,51 @@ import Header from './Header';
 function Login()
 {
 	const navigate=useNavigate();
+	const [email,setEmail]=useState("");
+	const [password,setPassword]=useState("");
+
 	useEffect(()=>{
 		if(localStorage.getItem('user-info')){
 			navigate('/add')	
 		}
 	},[])
+
+
+	async function login()
+	{
+		console.warn("data",email,password);
+		let userInfo = {email,password};		
+		let result = await fetch('http://localhost:8000/api/login',{
+			method:"post",
+			headers:{
+				"content-type":"application/json",
+				"Accept":"Application/JSON"
+			},
+			body:JSON.stringify(userInfo)
+		});
+		result=await result.json();
+		//console.warn(result);
+		localStorage.setItem("user-info",JSON.stringify(result));
+		navigate('/add');
+		
+
+	}
 	return(
 
 		<>
 			<Header />
+			<div className="col-sm-6 offset-sm-3">
+
 			<h1>Login Page</h1>
+			<br />
+			<input type="text" onChange={(e)=>setEmail(e.target.value)} placeholder="Enter email" className="form-control"></input>
+			<br />
+			<input type="text" onChange={(e)=>setPassword(e.target.value) }  placeholder="Enter password" className="form-control"></input>
+			<br />
+				
+			<button onClick={login} class="btn btn-success">Login</button>
+
+			</div>			
 		</>
 	)
 }
